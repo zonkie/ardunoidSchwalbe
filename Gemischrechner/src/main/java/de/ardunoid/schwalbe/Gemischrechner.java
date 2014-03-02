bd2b8f814adb6234bd56fe5af42da0c3dfd0ea3d
Soenke Domroese <soenke@soenke-domroese.de>
2014-03-02 21:55:22 +0100

    Gemischrechner

--- Gemischrechner/src/main/java/de/ardunoid/schwalbe/Gemischrechner.java
+++ Gemischrechner/src/main/java/de/ardunoid/schwalbe/Gemischrechner.java
@@ -1,18 +1,66 @@
 package de.ardunoid.schwalbe;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+//import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.SeekBar.*;
+import android.widget.TextView;
 
-public class Gemischrechner extends ActionBarActivity {
+
+public class Gemischrechner extends Activity {
+
+    private SeekBar mixBar;
+    private SeekBar gasolineBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gemischrechner);
-    }
 
+
+        mixBar = (SeekBar) findViewById(R.id.seekMix);
+        gasolineBar = (SeekBar) findViewById(R.id.seekGasoline);
+
+        OnSeekBarChangeListener seekbarChangeListener = new OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar mix) {}
+            @Override
+            public void onStartTrackingTouch(SeekBar mix) {}
+
+            @Override
+            public void onProgressChanged(SeekBar mix, int progress,boolean fromUser) {
+                calculateMix();
+            }
+        };
+        mixBar.setOnSeekBarChangeListener(seekbarChangeListener);
+        gasolineBar.setOnSeekBarChangeListener(seekbarChangeListener);
+
+    }
+    public void calculateMix(){
+        int progressMix = mixBar.getProgress();
+        int progressGasInt = gasolineBar.getProgress();
+        float progressGas = (float)progressGasInt;
+        progressGas = progressGas/100;
+        TextView mixLabel = (TextView)findViewById(R.id.textMixLabel);
+        TextView gasolineLabel = (TextView)findViewById(R.id.textGasolineLiter);
+
+        mixLabel.setText("1:" +  Integer.toString(progressMix));
+        gasolineLabel.setText(Float.toString(progressGas) + " Liter");
+
+        Log.d("Schwalbe", "Mix: " + String.valueOf(progressMix) + "Gasoline: " + String.valueOf(progressGas));
+        TextView textResult = (TextView) findViewById(R.id.textResult);
+        if(progressMix > 0 && progressGas > 0){
+            int oil = progressGasInt / progressMix;
+            float oilFloat = (float)oil;
+            oilFloat = oilFloat/100;
+            Log.d("Schwalbe", "Oil: " + Float.toString( oilFloat ));
+            textResult.setText(Float.toString(oilFloat) + "Liter");
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
